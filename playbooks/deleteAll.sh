9c926fa301a86c91f3821add41ff5c7b3dfe3acb
annadowling <anna.dowling@errigal.com>
2018-11-26 21:49:46 +0000

    Work on ansible

--- playbooks/deleteAll.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-ansible-playbook deleteS3Bucket.yml --vault-id $1-pass
-ansible-playbook deleteDynamodbTable.yml--vault-id $1-pass
-ansible-playbook deleteLambdaFunction.yml--vault-id $1-pass
