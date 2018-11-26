843069f3b00f41a1ae3f2e3fbc671d3a86c59805
annadowling <anna.dowling@errigal.com>
2018-11-26 22:07:35 +0000

    work on queuing

--- lambda_functions/receive-vote/receive-vote.js
+++ lambda_functions/receive-vote/receive-vote.js
@@ -1,37 +1,35 @@
 console.log('Loading event');
 var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB();
 
 exports.handler = function(event, context) {
-  //var twilio = require('twilio');
-  var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10', region: 'eu-west-1'});
-
   /* Make sure we have a valid vote (one of [RED, GREEN, BLUE]) */
   console.log(event);
-  var votedFor = event['Body'].toUpperCase().trim();
-  if (['RED', 'GREEN', 'BLUE'].indexOf(votedFor) >= 0) {
-    /* Add randomness to our value to help spread across partitions */
-    votedForHash = votedFor + "." + Math.floor((Math.random() * 10) + 1).toString();
-    /* ...updateItem into our DynamoDB database */
-    var tableName = 'VoteApp';
-    dynamodb.updateItem({
-      'TableName': tableName,
-      'Key': { 'VotedFor' : { 'S': votedForHash }},
-      'UpdateExpression': 'add #vote :x',
-      'ExpressionAttributeNames': {'#vote' : 'Votes'},
-      'ExpressionAttributeValues': { ':x' : { "N" : "1" } }
-    }, function(err, data) {
-      if (err) {
-        console.log(err);
-        context.fail(err);
-      } else {
-        //var resp = new twilio.TwimlResponse();
-        //resp.message("Thank you for casting a vote for " + votedFor);
-        context.done(null, "Thank you for casting a vote for " + votedFor);
-        console.log("Vote received for %s", votedFor);
-      }
+    event.Records.forEach(function (record) {
+        var votedFor = record['body'].toUpperCase().trim();
+        if (['RED', 'GREEN', 'BLUE'].indexOf(votedFor) >= 0) {
+            /* Add randomness to our value to help spread across partitions */
+            votedForHash = votedFor + "." + Math.floor((Math.random() * 10) + 1).toString();
+            /* ...updateItem into our DynamoDB database */
+            var tableName = 'VoteApp';
+            dynamodb.updateItem({
+                'TableName': tableName,
+                'Key': { 'VotedFor' : { 'S': votedForHash }},
+                'UpdateExpression': 'add #vote :x',
+                'ExpressionAttributeNames': {'#vote' : 'Votes'},
+                'ExpressionAttributeValues': { ':x' : { "N" : "1" } }
+            }, function(err, data) {
+                if (err) {
+                    console.log(err);
+                    context.fail(err);
+                } else {
+                    context.done(null, "Thank you for casting a vote for " + votedFor);
+                    console.log("Vote received for %s", votedFor);
+                }
+            });
+        } else {
+            console.log("Invalid vote received (%s)", votedFor);
+            context.fail("Invalid vote received");
+        }
     });
-  } else {
-    console.log("Invalid vote received (%s)", votedFor);
-    context.fail("Invalid vote received");
-  }
 }
